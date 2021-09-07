046d911f7784b6cd82733f5eccb47380a0f78300
AkiJoey <akijoey1010635951@gmail.com>
2021-09-07 15:28:17 +0800

    fix: release config

--- .releaserc.js
+++ .releaserc.js
@@ -1,5 +1,31 @@
 // .releaserc.js
 
-const config = require('@akijoey/semantic-release-config')
+// const config = require('@akijoey/semantic-release-config')
 
-module.exports = config.releasePackage('maven')
+// module.exports = config.releasePackage('maven')
+
+module.exports = {
+  branches: 'main',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    'semantic-release-maven',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json', 'pom.xml'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+      }
+    ],
+    '@semantic-release/github'
+  ],
+  prepare: [
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    'semantic-release-maven',
+    '@semantic-release/git'
+  ]
+}
